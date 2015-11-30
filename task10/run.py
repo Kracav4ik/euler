48e7ec017cd683920d29b9509fc644eeb5b5a2af
Kracav4ik <dikama2013@yandex.ru>
2015-12-01 01:22:53 +0300

    Задание 10

---
+++ task10/run.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+
+def is_prime(xx):
+    x2 = sqrt(xx)
+    for p in primes:
+        if xx % p == 0:
+            return False
+        if p > x2:
+            break
+    return True
+
+summ = 0
+x = 2
+primes = []
+while True:
+    if is_prime(x):
+        primes.append(x)
+        summ += x
+    x += 1
+    if x >= 2000000:
+        break
+print summ
