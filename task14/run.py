21bdacce5b5592a237c663db8db5c1027b7f8a6f
Kracav4ik <dikama2013@yandex.ru>
2015-12-01 04:25:36 +0300

    Задание 14 самая длинная последовательность Колатца чисел до 1000000

--- task14/run.py
+++ task14/run.py
@@ -7,6 +7,16 @@ def collatz(x):
         if x % 2 == 0:
             x /= 2
         else:
-            x = 3*x + 1
+            x = 3 * x + 1
     R.append(1)
     return R
+
+
+v = 1
+d_max = 1
+for x in range(1000000):
+    coll_len = len(collatz(x))
+    if coll_len > d_max:
+        d_max = coll_len
+        v = x
+print v
